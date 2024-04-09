3d3ce3ff982303b2dbf6bdc916177c61f3a25785
MéLoBeats <lamelobeats@gmail.com>
2024-04-09 20:35:03 +0200

    fix: refactor down

--- html js/index.js
+++ html js/index.js
@@ -1,28 +1,7 @@
-var cubeBtn = document.getElementById("cube");
-var rightCol = document.getElementById("right-col");
-var btnStack = document.getElementById("btn-stack");
-var background= document.getElementById("bg");
+let btnStack = document.getElementById('btn-stack');
+let animateBackground = document.getElementById('animateBackground');
 
-//je veux que au clique me bouton devienne rouge
-
-btnStack.onclick = function () {
-
-    if (btnStack.textContent === "Mes stacks") {
-        btnStack.textContent = "fermer";
-    } else {    
-        btnStack.textContent = "Mes stacks";
-    }
-    if
-    (background.style.display === "none"){
-        background.style.display = "block";}
-        else{
-            background.style.display = "none";
-        }
-        
-     
-    };
-
-
-
-
-console.log(btnStack);
+btnStack.addEventListener('click', function(){
+    animateBackground.classList.toggle('bg-animate-none');
+    btnStack.textContent = stack.classList.contains('visible') ? 'Fermer' : 'Mes Stacks';
+});    
